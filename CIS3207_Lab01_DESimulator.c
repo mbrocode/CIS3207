595639ff9bd5700ed12cca06811a63ec14ef6376
mbrocode <48998172+mbrocode@users.noreply.github.com>
2019-09-14 14:16:38 -0400

    Add files via upload

--- CIS3207_Lab01_DESimulator.c
+++ CIS3207_Lab01_DESimulator.c
@@ -15,14 +15,14 @@
 //GLOBAL VARIABLES-------------------------------------------------------------------------------------
 
 int SEED = 0;
-int INIT_TIME = 0;
-int  FIN_TIME = 0;
-int ARRIVE_MIN = 0;
+int INIT_TIME = 0;      //Beginning of simulation
+int FIN_TIME = 0;       //End of Simulation
+int ARRIVE_MIN = 0;     //New process will enter sys at rand interval between min & max
 int ARRIVE_MAX = 0;
-double QUIT_PROB = 0;
-int CPU_MIN = 0;
+double QUIT_PROB = 0;   //Prob process will exit sys after done executing on CPU
+int CPU_MIN = 0;        //Time a process spends executing on CPU rand interval between min & max
 int CPU_MAX = 0;
-int DISK1_MIN = 0;
+int DISK1_MIN = 0;      //Time a process spends doing I/O on Disk rand interval between min & max
 int DISK1_MAX = 0;
 int DISK2_MIN = 0;
 int DISK2_MAX = 0;
@@ -30,11 +30,64 @@ int DISK2_MAX = 0;
 
 //PROTOTYPE FUNCTIONS----------------------------------------------------------------------------------
 
-//HANDLERS:
 
 //MAIN-------------------------------------------------------------------------------------------------
 int main() {
 
+    //PART 1: Read variables from config.txt____________________________
+    FILE *fptr;
+    char varStrValues[12];
+    int varValues[12] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+    int i = 0, j = 0;
+
+    if ((fptr = fopen("config.txt","r")) == NULL){
+       printf("Error! opening file");
+       exit(1);
+    }
+
+    while(fscanf(fptr, "%*s %s", varStrValues) == 1){
+        if(i==5){
+            sscanf(varStrValues, "%f", &QUIT_PROB);
+        }
+        sscanf(varStrValues, "%d", &varValues[i++]);
+        printf("%s\n", varStrValues);
+    }
+
+    for(j=0; j<12; j++){
+        printf("\n%d",varValues[j]);
+    }
+
+    SEED = varValues[0];
+    INIT_TIME = varValues[1];
+    FIN_TIME = varValues[2];
+    ARRIVE_MIN = varValues[3];
+    ARRIVE_MAX = varValues[4];
+    CPU_MIN = varValues[6];
+    CPU_MAX = varValues[7];
+    DISK1_MIN = varValues[8];
+    DISK1_MAX = varValues[9];
+    DISK2_MIN = varValues[10];
+    DISK2_MAX = varValues[11];
+
+    printf("\n\nSEED = %d", SEED);
+    printf("\nINIT_TIME = %d", INIT_TIME);
+    printf("\nFIN_TIME = %d", FIN_TIME);
+    printf("\nARRIVE_MIN = %d", ARRIVE_MIN);
+    printf("\nARRIVE_MAX = %d", ARRIVE_MAX);
+    printf("\nQUIT_PROB = %2.1f", QUIT_PROB);
+    printf("\nCPU_MIN = %d", CPU_MIN);
+    printf("\nCPU_MAX = %d", CPU_MAX);
+    printf("\nDISK1_MIN = %d", DISK1_MIN);
+    printf("\nDISK1_MAX = %d", DISK1_MAX);
+    printf("\nDISK2_MIN = %d", DISK2_MIN);
+    printf("\nDISK2_MAX = %d", DISK2_MAX);
+
+    fclose(fptr);
+
+
+
+    /*
+    //Loop to execute EventQueue
     while (EventQueue is not empty ) {
 
         event = EventQueue.pop();
@@ -51,4 +104,7 @@ int main() {
             //add more cases...
         }
     }
+    */
 }
+
+
